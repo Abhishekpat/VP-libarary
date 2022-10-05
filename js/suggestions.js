1d5df8c79f53a556a034eba5cc5b3c4bb4f8f856
aqeeb-pathan <112194641+aqeeb-pathan@users.noreply.github.com>
2022-10-05 11:37:58 +0530

    Initial commit

---
+++ js/suggestions.js
@@ -0,0 +1,8 @@
+let suggestions = [
+    "Computer Engineering",
+    "Information Technology",
+    "Electronics and Telecommunications",
+    "Question paper",
+    "Modal answer",
+    "Textbox",
+]
